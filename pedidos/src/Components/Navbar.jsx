1e10e510340c441f521c1cb609b20a187748faa4
RaymelDev <raimelantuna24@gmail.com>
2026-02-01 21:17:51 -0400

    Refactor Navbar component for mobile responsiveness

--- pedidos/src/Components/Navbar.jsx
+++ pedidos/src/Components/Navbar.jsx
@@ -2,17 +2,27 @@ import { NavLink ,Link } from "react-router-dom";
 import { ThemeContext } from "../context/ThemeContext";
 import { useContext } from "react";
 import './navbar.css'
+import { useState } from "react";
 
 function Navbar(){
   const { toggleTheme, theme } = useContext(ThemeContext);
+  const [menu, setMenu] = useState(false)
+  
     return(
       <nav className="navbar">
-          <ul className="list-links">
-            <li><NavLink to="/" end className="nav-link">Inicio</NavLink></li>
-            <li><NavLink to="/pedidos" className="nav-link">Pedidos</NavLink></li>
-            <li><NavLink to="/crear-pedido" className="nav-link">Crear pedido</NavLink></li>
+        <button className="menu-btn" onClick={() => setMenu(!menu)}>☰</button>
+          <ul className={`list-links ${menu ? "open" : ""}`}>
+            <li><NavLink to="/" end className="nav-link" onClick={() => setMenu(false)}>Inicio</NavLink></li>
+            <li><NavLink to="/pedidos" className="nav-link" onClick={() => setMenu(false)}>Pedidos</NavLink></li>
+            <li><NavLink to="/crear-pedido" className="nav-link" onClick={()=> setMenu(false)}>Crear pedido</NavLink></li>
+            <li className="mobile-actions">
+              <button className="btn-theme" onClick={toggleTheme}>
+                {theme === "dark" ? "☀️" : "🌙"}
+              </button>
+              <Link to="/crear-pedido" className="btn-agregar">+</Link>
+            </li>
           </ul>
-        <div className="nav-controls">
+        <div className="desktop-actions">
           <button className="btn-theme" onClick={toggleTheme}>
             {theme === "dark" ? "☀️" : "🌙"}
           </button>
